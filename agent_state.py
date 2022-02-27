3a0b178432364d1667890acb44c022fac09b15ce
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-02-26 19:25:47 -0600

    Agente que guarda estados

--- agent_state.py
+++ agent_state.py
@@ -5,35 +5,37 @@ from actions import *
 
 
 class AgentState(Agent):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self,
+                 env: Environment,
+                 x_pos: int = 0, y_pos: int = 0) -> None:
+        super().__init__(env, x_pos, y_pos)
         self.visited = set()
         self.dirty_rooms = set()
 
-    def action(self, action: str, env: Environment) -> None:
-        super().action(action, env)
+    def action(self, action: str) -> None:
+        super().action(action)
         self.visited.add((self.x, self.y))
 
    # Sensor del ambiente
-    def perceive(self, env: Environment) -> None:
-        self.dirty = env.dirt_amount(self.x, self.y) > 0
+    def perceive(self) -> None:
+        self.dirty = self.env.dirt_amount(self.x, self.y) > 0
         dirty_rooms = set()
         self.valid_moves.clear()
         if self.x-1 >= 0:
             self.valid_moves.add(UP)
-            if env.dirt_amount(self.x-1, self.y) > 0:
+            if self.env.dirt_amount(self.x-1, self.y) > 0:
                 dirty_rooms.add(UP)
-        if self.x+1 < env.rows:
+        if self.x+1 < self.env.rows:
             self.valid_moves.add(DOWN)
-            if env.dirt_amount(self.x+1, self.y) > 0:
+            if self.env.dirt_amount(self.x+1, self.y) > 0:
                 dirty_rooms.add(DOWN)
         if self.y-1 >= 0:
             self.valid_moves.add(LEFT)
-            if env.dirt_amount(self.x, self.y-1) > 0:
+            if self.env.dirt_amount(self.x, self.y-1) > 0:
                 dirty_rooms.add(LEFT)
-        if self.y+1 < env.cols:
+        if self.y+1 < self.env.cols:
             self.valid_moves.add(RIGHT)
-            if env.dirt_amount(self.x, self.y+1) > 0:
+            if self.env.dirt_amount(self.x, self.y+1) > 0:
                 dirty_rooms.add(RIGHT)
         dirty_rooms.intersection_update(self.not_visited())
         if len(dirty_rooms) > 0:
