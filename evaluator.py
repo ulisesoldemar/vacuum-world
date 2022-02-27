bc14ae2fe7d0d972ed0fa84643d89334049da1b0
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-02-26 19:26:42 -0600

    Modificado parámetro de performance

--- evaluator.py
+++ evaluator.py
@@ -1,19 +1,23 @@
-from environment import Environment
 from actions import *
-
+import time
 
 class Evaluator:
     def __init__(self) -> None:
-        self.dirty_degree = 0
         self.consumed_energy = 0
+        self.start_time = 0
+        self.stop_time = 0
 
-    def eval(self, action: str, env: Environment) -> None:
+    def eval(self, action: str) -> None:
         if action == SUCK:
             self.consumed_energy += 2
         elif action != IDLE:
             self.consumed_energy += 1
+    
+    def start(self) -> float:
+        self.start_time = time.perf_counter()
+        return self.start_time
 
-        for x in range(env.rows):
-            for y in range(env.cols):
-                da = env.dirt_amount(x, y)
-                self.dirty_degree += da*da
+    def stop(self) -> float:
+        self.stop_time = time.perf_counter() - self.start_time
+        return self.stop_time
+
