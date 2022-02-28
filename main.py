a9ca833bd409b310512f0f76f01d29c442ada1b5
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-02-27 19:28:30 -0600

    Posibilidad de guardar promedios en un archivo json

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ from agent_state import AgentState
 from environment import Environment
 from pprint import pprint
 import argparse
+import json
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Mundo de la aspiradora.')
-    parser.add_argument('FILE', type=str, help='archivo con el layout del mundo en formato JSON')
-    parser.add_argument('-x', type=int, help='posición en x de la aspiradora', default=0)
-    parser.add_argument('-y', type=int, help='posición en y de la aspiradora', default=0)
-    parser.add_argument('--agent', type=str, help='tipo de agente; valores aceptados simple (default), state y penalized', default='simple')
+    parser.add_argument(
+        'FILE', type=str, help='archivo con el layout del mundo en formato JSON')
+    parser.add_argument(
+        '-x', type=int, help='posición en x de la aspiradora', default=0)
+    parser.add_argument(
+        '-y', type=int, help='posición en y de la aspiradora', default=0)
+    parser.add_argument(
+        '--agent', type=str, help='tipo de agente; valores aceptados simple (default), state y penalized', default='simple')
+    parser.add_argument('--save', type=str,
+                        help='archivo JSON donde se guardará la salida')
     args = parser.parse_args()
     show(args)
+
 
 def show(args: argparse.Namespace) -> None:
     env = Environment.load_layout(args.FILE)
@@ -35,6 +44,34 @@ def show(args: argparse.Namespace) -> None:
         print('los valores de x y y deben estar en los límites del layout del archivo:')
         pprint(env.layout)
 
+    if args.save:
+        dump(args.save, args, agent.log)
+
+
+def dump(filename: str, args: argparse.Namespace, log: dict) -> None:
+    try:
+        with open(filename, 'r') as json_file:
+            data = json.load(json_file)
+
+        data[args.FILE]['Agent type'] = args.agent
+        data[args.FILE]['Run count'] += 1
+        data[args.FILE]['Total performance'] += log['Performance']
+        data[args.FILE]['Average performance'] = data[args.FILE]['Total performance'] / \
+            data[args.FILE]['Run count']
+
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file)
+
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        data = dict()
+        log['Agent type'] = args.agent
+        log['Run count'] = 1
+        log['Total performance'] = log['Performance']
+        log['Average performance'] = log['Performance']
+        data[args.FILE] = log
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file)
+
+
 if __name__ == '__main__':
     main()
-    
